1c4629202233dfaef9f4e2c408bb6ffa3bb12aea
Pradip Mishra <pradipmishra9863@gmail.com>
2026-08-03 12:30:05 +0545

    Setup done

---
+++ src/main/java/com/project/digitalwallet/DigitalwalletApplication.java
@@ -0,0 +1,13 @@
+package com.project.digitalwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalwalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalwalletApplication.class, args);
+	}
+
+}
