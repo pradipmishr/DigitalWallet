f2fc36e6f9132202f04cfb6688aa192d8c2e8624
Pradip Mishra <pradipmishra9863@gmail.com>
2026-08-05 15:30:28 +0545

    refactor: removed unnecessary methods

--- src/main/java/com/project/digitalwallet/entity/BaseEntity.java
+++ src/main/java/com/project/digitalwallet/entity/BaseEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -19,16 +20,7 @@ public abstract class BaseEntity {
     @CreationTimestamp
     private LocalDateTime createdAt;
 
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 
-    @PrePersist
-    protected void onCreate() {
-        createdAt = LocalDateTime.now();
-        updatedAt = LocalDateTime.now();
-    }
-
-    @PreUpdate
-    protected void onUpdate() {
-        updatedAt = LocalDateTime.now();
-    }
 }
